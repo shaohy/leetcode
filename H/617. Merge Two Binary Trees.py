b60a1ce61b9620bc4a4a67b0b125fb04458bb618
Huiyu Shao <hyshao615@gmail.com>
2019-04-02 23:13:40 +1300

    02/04/2019

--- H/617. Merge Two Binary Trees.py
+++ H/617. Merge Two Binary Trees.py
@@ -7,3 +7,11 @@
 
 class Solution:
     def mergeTrees(self, t1: TreeNode, t2: TreeNode) -> TreeNode:
+        if t1 is None:
+            return t2
+        elif t2 is None:
+            return t1
+        t1.val += t2.val
+        t1.left = mergeTrees(t1.left, t2.left)
+        t1.right = mergeTrees(t1.right, t2.right)
+        return t1
