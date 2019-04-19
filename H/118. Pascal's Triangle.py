c256852c4095cf22913f2f8afb3861f72865449c
Huiyu Shao <hyshao615@gmail.com>
2019-04-19 23:23:57 +1200

    19/04/2019

--- H/118. Pascal's Triangle.py
+++ H/118. Pascal's Triangle.py
@@ -1,4 +1,11 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        
+        triangle = []
+        for i in range(numRows):
+            row = []
+            row[0], row[-1] = [1, 1]
+            for j in range(1,len(row)-1):
+                row[j] = triangle[numRows-1][j-1] + triangle[numRows-1][j]
+            triangle.append(row)
+        return triangle
 
