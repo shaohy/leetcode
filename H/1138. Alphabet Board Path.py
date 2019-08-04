fd38d88766a3020504115a2165245973ad389918
Huiyu Shao <hyshao615@gmail.com>
2019-08-04 23:51:52 +1200

    Update 1138. Alphabet Board Path.py

--- H/1138. Alphabet Board Path.py
+++ H/1138. Alphabet Board Path.py
@@ -1,16 +1,26 @@
 class Solution:
     def alphabetBoardPath(self, target: str) -> str:
+        target = "a" + target
         board = ["abcde", "fghij", "klmno", "pqrst", "uvwxy", "z"]
         output = ""
-        target = "aj"
         alpha_dic = {}
-        start = (0, 0)  # a
-        end = (1, 4)  # j
         for i in range(len(board)):
             for j in range(len(board[i])):
                 alpha_dic[board[i][j]] = (i, j)
 
+        for i in range(len(target) - 1):
+
+            if alpha_dic[target[i]][0] > alpha_dic[target[i + 1]][0]:
+                output += "U" * abs(alpha_dic[target[i]][0] - alpha_dic[target[i + 1]][0])
+            if alpha_dic[target[i]][0] < alpha_dic[target[i + 1]][0]:
+                output += "D" * abs(alpha_dic[target[i]][0] - alpha_dic[target[i + 1]][0])
+            if alpha_dic[target[i]][0] > alpha_dic[target[i + 1]][0]:
+                output += "R" * abs(alpha_dic[target[i]][1] - alpha_dic[target[i + 1]][1])
+            if alpha_dic[target[i]][0] < alpha_dic[target[i + 1]][0]:
+                output += "L" * abs(alpha_dic[target[i]][1] - alpha_dic[target[i + 1]][1])
+            output += "!"
+        return output
 
 
 test = Solution()
-test.alphabetBoardPath("leet")
+print(test.alphabetBoardPath("leet"))
