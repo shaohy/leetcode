a05abf605481d5d4e68c40a205198b6cac94f4fe
Huiyu Shao <hyshao615@gmail.com>
2019-08-05 16:16:55 +1200

    Update 1138. Alphabet Board Path.py

--- H/1138. Alphabet Board Path.py
+++ H/1138. Alphabet Board Path.py
@@ -7,20 +7,31 @@ class Solution:
         for i in range(len(board)):
             for j in range(len(board[i])):
                 alpha_dic[board[i][j]] = (i, j)
+        print(alpha_dic)
 
         for i in range(len(target) - 1):
+            a = target[i]
+            b = target[i+1]
+            if a == b:
+                output += "!"
+                continue
+            if target[i+1] == "z":
+                b = "u"
 
-            if alpha_dic[target[i]][0] > alpha_dic[target[i + 1]][0]:
-                output += "U" * abs(alpha_dic[target[i]][0] - alpha_dic[target[i + 1]][0])
-            if alpha_dic[target[i]][0] < alpha_dic[target[i + 1]][0]:
-                output += "D" * abs(alpha_dic[target[i]][0] - alpha_dic[target[i + 1]][0])
-            if alpha_dic[target[i]][1] > alpha_dic[target[i + 1]][1]:
-                output += "L" * abs(alpha_dic[target[i]][1] - alpha_dic[target[i + 1]][1])
-            if alpha_dic[target[i]][1] < alpha_dic[target[i + 1]][1]:
-                output += "R" * abs(alpha_dic[target[i]][1] - alpha_dic[target[i + 1]][1])
+            if alpha_dic[a][0] > alpha_dic[b][0]:
+                output += "U" * abs(alpha_dic[a][0] - alpha_dic[b][0])
+            if alpha_dic[a][0] < alpha_dic[b][0]:
+                output += "D" * abs(alpha_dic[a][0] - alpha_dic[b][0])
+            if alpha_dic[a][1] > alpha_dic[b][1]:
+                output += "L" * abs(alpha_dic[a][1] - alpha_dic[b][1])
+            if alpha_dic[a][1] < alpha_dic[b][1]:
+                output += "R" * abs(alpha_dic[a][1] - alpha_dic[b][1])
+            if target[i+1] == "z":
+                output += "D"
             output += "!"
         return output
 
 
 test = Solution()
-print(test.alphabetBoardPath("code"))
+print(test.alphabetBoardPath("zzz"))
+print(test.alphabetBoardPath("leet"))
